4018eb0b6a2be4705bd0e19a99dfded4404c7b5e
gmy007 <46482772+gmy007@users.noreply.github.com>
2020-09-10 17:40:21 +0800

    实验室提交

---
+++ src/main/java/com/cug/leetcode/Demo165.java
@@ -0,0 +1,34 @@
+package com.cug.leetcode;
+
+public class Demo165 {
+    public static void main(String[] args) {
+        System.out.println(Integer.parseInt("01"));
+    }
+    public int compareVersion(String version1, String version2) {
+        String[] split1 = version1.split("\\.");
+        String[] split2 = version2.split("\\.");
+        int i=0,j=0;
+        while(i<split1.length && j < split2.length){
+            int num1 = Integer.parseInt(split1[i]);
+            int num2 = Integer.parseInt(split2[j]);
+            if(num1 < num2){
+                return -1;
+            }else if(num1 > num2)
+                return 1;
+            i++;
+            j++;
+        }
+        while(i>=split1.length && j< split2.length) {
+            if(Integer.parseInt(split2[j++]) == 0)
+                continue;
+            return -1;
+        }
+        while(i<split1.length && j>= split2.length) {
+            if(Integer.parseInt(split1[i++]) == 0)
+                continue;
+            return 1;
+        }
+        return 0;
+
+    }
+}
